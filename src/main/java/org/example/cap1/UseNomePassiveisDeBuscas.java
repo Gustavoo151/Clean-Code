993a62cd20eefce0d0ff8f351aa78d0fc6ab934d
gustavoo151 <josegustavooliveira121@gmail.com>
2025-08-06 05:10:52 -0300

    Add UseNomePassiveisDeBuscas class with examples of clear and unclear code

---
+++ src/main/java/org/example/cap1/UseNomePassiveisDeBuscas.java
@@ -0,0 +1,20 @@
+package org.example.cap1;
+
+public class UseNomePassiveisDeBuscas {
+/*    Exemplo de código que não é claro, pois não sabemos o que é t e s, e o que eles representam.
+      for (int j=0; j<34; j++){
+          s += (t[j] *4)/5;
+    }
+*/
+    /* Exemplo de código que é claro, pois sabemos o que é t e s, e o que eles representam.
+    int  realDaysPerIdealDay = 4;
+    const int WORK_DAYS_PER_WEEK = 5;
+    int sum = 0;
+
+    for (int j=0; j < NUMBER_OF_TASK; j++){
+        int realTaskDay = taskEstimete[j] * realDaysPerIdealDay;
+        int realTaskWeek = (realDays / WORK_DAYS_PER_WEEK);
+        sum += realTaskWeek;
+    }
+   */
+}
